056aac251a7906fe51f59e01ce415ae83d998926
sorensej <sorensej@rose-hulman.edu>
2020-07-19 23:53:09 -0500

    Finished up the dashboard.

--- app/src/main/java/edu/rosehulman/gearlocker/dashboard/DashboardFragment.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/dashboard/DashboardFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import edu.rosehulman.gearlocker.R
 
@@ -21,11 +19,6 @@ class DashboardFragment : Fragment() {
     ): View? {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 }
