9296a112bd8c91e6b9c7db37252d18a7cec8f989
sorensej <sorensej@rose-hulman.edu>
2020-07-27 21:52:03 -0500

    Added the checkout cardview, rental home fragment, and worked on the mobile navigation.

--- app/src/main/java/edu/rosehulman/gearlocker/management/ManagementSignInFragment.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/management/ManagementSignInFragment.kt
@@ -1,4 +1,4 @@
-package edu.rosehulman.gearlocker.rentals
+package edu.rosehulman.gearlocker.management
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,22 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import edu.rosehulman.gearlocker.ManagementMainActivity
 import edu.rosehulman.gearlocker.R
-import edu.rosehulman.gearlocker.dashboard.DashboardFragment
 import kotlinx.android.synthetic.main.gear_management_sign_in.view.*
 
 class ManagementSignInFragment: Fragment() {
-
-    var mainActivity: ManagementHomeFragment.FragmentSwitcher? = null
-
-    fun newInstance(mainActivity: ManagementHomeFragment.FragmentSwitcher?): ManagementSignInFragment {
-        val m = ManagementSignInFragment()
-        m.mainActivity = mainActivity
-        return m
-    }
-
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,7 +24,7 @@ class ManagementSignInFragment: Fragment() {
             startActivity(Intent(this.context, ManagementMainActivity::class.java))
         }
         view.return_to_dash_button_login.setOnClickListener {
-            mainActivity?.switchToFragment(DashboardFragment())
+            findNavController().navigate(R.id.navigation_dashboard)
         }
         return view
     }
