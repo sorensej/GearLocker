26be34d28f4dcd8152304ba2eb89bf21ab98e75f
sorensej <sorensej@rose-hulman.edu>
2020-07-29 13:00:45 -0500

    Got rid of unused files

--- app/src/main/java/edu/rosehulman/gearlocker/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.renter_activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         appBarConfiguration = AppBarConfiguration(setOf(
