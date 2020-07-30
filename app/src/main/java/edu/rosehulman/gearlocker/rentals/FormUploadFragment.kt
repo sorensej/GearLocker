3f0adbaeeb039c00276b61be9d7f1078b61565ad
Ben Gothard <ben.gothard3@gmail.com>
2020-07-30 13:53:02 -0400

    Added form upload fragment

---
+++ app/src/main/java/edu/rosehulman/gearlocker/rentals/FormUploadFragment.kt
@@ -0,0 +1,20 @@
+package edu.rosehulman.gearlocker.rentals
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import edu.rosehulman.gearlocker.R
+
+class FormUploadFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_upload_form, container, false)
+        return view
+    }
+}
