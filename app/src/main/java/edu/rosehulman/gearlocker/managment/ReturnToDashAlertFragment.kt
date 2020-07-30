3746fcfc3ad0585e3b776495aff192453e6f0af5
sorensej <sorensej@rose-hulman.edu>
2020-07-29 20:43:31 -0500

    Messed around with nav graph/bottom nav popping up an alert.

--- app/src/main/java/edu/rosehulman/gearlocker/managment/ReturnToDashAlertFragment.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/managment/ReturnToDashAlertFragment.kt
@@ -3,15 +3,18 @@ package edu.rosehulman.gearlocker.managment
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
+import edu.rosehulman.gearlocker.Constants
 import edu.rosehulman.gearlocker.R
 
 
 class ReturnToDashAlertFragment: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val title = "Log out of management to return to dash?"
-        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(context)
+        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(activity)
+        Log.d(Constants.TAG, "Activity: ${activity?.supportFragmentManager?.popBackStack()}")
         alertDialogBuilder.setTitle(title)
 
         alertDialogBuilder.setPositiveButton("OK"
