44849ff42bcc74aec0db483f0c70b2e4d4cccfd1
Ben Gothard <ben.gothard3@gmail.com>
2020-08-03 13:48:13 -0400

    Added object to provide demo data and added fromSnapshot() methods to each model class

--- app/src/main/java/edu/rosehulman/gearlocker/models/Club.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/models/Club.kt
@@ -1,5 +1,18 @@
 package edu.rosehulman.gearlocker.models
 
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Exclude
+
 data class Club (
     var name: String = ""
-)
+) {
+    @get:Exclude var id: String = ""
+
+    companion object {
+        fun fromSnapshot(doc: DocumentSnapshot): Club {
+            val c = doc.toObject(Club::class.java)!!
+            c.id = doc.id
+            return c
+        }
+    }
+}
