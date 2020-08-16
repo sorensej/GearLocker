370eba4b7fec62e98b3cf44e7ef5e55579c844c1
Ben Gothard <ben.gothard3@gmail.com>
2020-08-16 17:53:23 -0400

    Fixed Firebase storage issues with AddItem.kt

--- app/src/main/java/edu/rosehulman/gearlocker/inventory/AddItem.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/inventory/AddItem.kt
@@ -4,37 +4,46 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
 import edu.rosehulman.gearlocker.Constants
-import edu.rosehulman.gearlocker.ImageProducer
 import edu.rosehulman.gearlocker.R
 import edu.rosehulman.gearlocker.models.Item
 import kotlinx.android.synthetic.main.add_edit_management.view.*
 import kotlinx.android.synthetic.main.add_item_confirmation.view.*
+import java.io.ByteArrayOutputStream
+import kotlin.math.abs
+import kotlin.random.Random
 
 class AddItem : Fragment() {
 
     private val RC_TAKE_PICTURE = 1
     private val RC_CHOOSE_PICTURE = 2
 
-    private val imageProducer = ImageProducer(this)
-
     private val itemCategoriesRef = FirebaseFirestore
         .getInstance()
         .collection(Constants.FB_ITEM_CATEGORIES)
+
+    private val storageRef = FirebaseStorage.getInstance().reference.child("images")
+    private var imageUri: String? = null
 
     @SuppressLint("ResourceType")
     override fun onCreateView(
@@ -48,19 +57,17 @@ class AddItem : Fragment() {
         activity?.findNavController(R.id.nav_host_fragment_management)?.graph?.startDestination =
             R.id.addItem
         view.upload_from_device.setOnClickListener {
-            imageProducer.launchChooseIntent()
-            addAndShowImage(view, imageProducer)
+            startPickActivity()
         }
         view.upload_from_camera.setOnClickListener {
-            imageProducer.launchCameraIntent()
-            addAndShowImage(view, imageProducer)
+            startCameraActivity()
         }
         if (!args.isAdd) {
             val item = args.item
             view.name_of_gear_edittext.setText(item!!.name)
-            view.price_textview.setText(item!!.estimatedCost.toString())
-            view.seekBar.progress = item!!.condition
-            view.description_edittext.setText(item!!.description)
+            view.price_textview.setText(item.estimatedCost.toString())
+            view.seekBar.progress = item.condition
+            view.description_edittext.setText(item.description)
         }
         view.submit_button.setOnClickListener {
             try {
@@ -70,7 +77,7 @@ class AddItem : Fragment() {
                     view.seekBar.progress,
                     view.description_edittext.text.toString(),
                     view.category_spinner.selectedItem.toString(),
-                    imageProducer.currentPhotoPath
+                    imageUri!!
                 )
                 args.itemInterface.onItemAdded(item)
                 val alertView =
@@ -96,7 +103,7 @@ class AddItem : Fragment() {
                 val builder = AlertDialog.Builder(activity)
                 builder.setTitle("Missing Input")
                 builder.setMessage("Please review your item change or addition.")
-                builder.setPositiveButton("OK") { dialog, which ->
+                builder.setPositiveButton("OK") { dialog, _ ->
                     dialog.dismiss()
                 }
                 builder.create().show()
@@ -115,24 +122,50 @@ class AddItem : Fragment() {
 
     }
 
-    private fun addAndShowImage(view: View, imageProducer: ImageProducer) {
-        Log.d(Constants.TAG, "Add image: ${imageProducer.currentPhotoPath}")
-        imageProducer.add(imageProducer.currentPhotoPath)
-        Log.d(Constants.TAG, "DownloadUri: ${imageProducer.downloadUri}")
-        Picasso.get().load(imageProducer.downloadUri).into(view.gear_image)
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
-                RC_TAKE_PICTURE   -> {
-                    imageProducer.sendCameraPhotoToAdapter()
+                RC_TAKE_PICTURE -> {
+                    addToStorage(data?.extras?.get("data") as Bitmap)
                 }
                 RC_CHOOSE_PICTURE -> {
-                    imageProducer.sendGalleryPhotoToAdapter(data)
+                    val stream = context?.contentResolver?.openInputStream(data?.data!!)
+                    addToStorage(BitmapFactory.decodeStream(stream))
                 }
             }
-            view?.gear_image?.setImageURI(imageProducer.currentPhotoPath.toUri())
+        }
+    }
+
+    private fun startCameraActivity() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (cameraIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(cameraIntent, RC_TAKE_PICTURE)
+        }
+    }
+
+    private fun startPickActivity() {
+        val pickIntent = Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        pickIntent.addCategory(Intent.CATEGORY_OPENABLE)
+        pickIntent.type = "image/*"
+        if (pickIntent.resolveActivity(requireContext().packageManager) != null) {
+            startActivityForResult(pickIntent, RC_CHOOSE_PICTURE)
+        }
+    }
+
+    private fun addToStorage(bitmap: Bitmap?) {
+        val baos = ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val id = abs(Random.nextLong()).toString()
+
+        val uploadTask = storageRef.child(id).putBytes(data)
+
+        uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> {
+            return@Continuation storageRef.child(id).downloadUrl
+        }).addOnSuccessListener { uri: Uri? ->
+            Picasso.get().load(uri).into(view?.gear_image)
+            this.imageUri = uri.toString()
         }
     }
 }
