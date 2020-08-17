72da1fb5cfe0d5bee74563de51cfeccf1d024b87
Ben Gothard <ben.gothard3@gmail.com>
2020-08-17 11:17:57 -0400

    Implemented joining/leaving clubs

--- app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsAdapter.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsAdapter.kt
@@ -6,12 +6,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.*
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 import edu.rosehulman.gearlocker.Constants
 import edu.rosehulman.gearlocker.R
-import edu.rosehulman.gearlocker.dashboard.DashboardViewHolder
 import edu.rosehulman.gearlocker.models.Club
-import edu.rosehulman.gearlocker.models.Rental
 
 
 class ClubsAdapter(private val context: Context
@@ -59,7 +60,7 @@ class ClubsAdapter(private val context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClubsViewHolder {
         val view: CardView=
             LayoutInflater.from(context).inflate(R.layout.club_cardview, parent, false) as CardView
-        return ClubsViewHolder(view)
+        return ClubsViewHolder(view, context)
     }
 
     override fun getItemCount(): Int= clubs.size
--- app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsViewHolder.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsViewHolder.kt
@@ -1,30 +1,73 @@
 package edu.rosehulman.gearlocker.clubs
 
-import android.annotation.SuppressLint
+import android.content.Context
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import edu.rosehulman.gearlocker.Constants
 import edu.rosehulman.gearlocker.R
 import edu.rosehulman.gearlocker.models.Club
-import edu.rosehulman.gearlocker.models.Item
-import edu.rosehulman.gearlocker.models.Rental
 import kotlinx.android.synthetic.main.club_cardview.view.*
 
 
-class ClubsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+class ClubsViewHolder(itemView: View, val context: Context): RecyclerView.ViewHolder(itemView){
     val button: TextView = itemView.add_club_textbutton
     private val clubText: TextView = itemView.club_textview
 
-    init{
-        button.setOnClickListener {
-            Log.d(Constants.TAG, "Clicked")
-        }
-    }
+    private val auth = FirebaseAuth.getInstance().currentUser
 
     fun bind(club: Club){
         clubText.text = club.name
+
+        if (club.members.containsValue(auth?.uid)) {
+            button.text = context.getString(R.string.leave_club)
+            button.setBackgroundResource(R.drawable.round_button_red)
+            button.setOnClickListener {
+                removeFromClub(club)
+            }
+        } else {
+            button.text = context.getString(R.string.add_club)
+            button.setBackgroundResource(R.drawable.round_button)
+            button.setOnClickListener {
+                addToClub(club)
+            }
+        }
+    }
+
+    private fun addToClub(club: Club) {
+        if (auth != null) {
+            val displayName = if (!auth.displayName.isNullOrEmpty()) { auth.displayName!! } else { auth.uid }
+            club.members.putIfAbsent(displayName, auth.uid)
+            Log.d(Constants.TAG, club.members.toString())
+            FirebaseFirestore
+                .getInstance()
+                .collection(Constants.FB_CLUBS)
+                .document(club.id)
+                .set(club)
+
+
+        }
+    }
+
+    private fun removeFromClub(club: Club) {
+        if (auth != null) {
+            val displayName = if(!auth.displayName.isNullOrEmpty()) { auth.displayName!! } else { auth.uid }
+
+            if (!club.members.containsKey(displayName)) {
+                return
+            }
+
+            club.members.remove(displayName)
+            Log.d(Constants.TAG, club.members.toString())
+            FirebaseFirestore
+                .getInstance()
+                .collection(Constants.FB_CLUBS)
+                .document(club.id)
+                .set(club)
+        }
     }
 
 }
