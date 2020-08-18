3c94e886708999ec6d9579232eca53ee51f0877a
sorensej <sorensej@rose-hulman.edu>
2020-08-17 19:31:59 -0500

    Still working on the search view. Not working yet, unfortunately.

--- app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsAdapter.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsAdapter.kt
@@ -2,22 +2,22 @@ package edu.rosehulman.gearlocker.clubs
 
 import android.content.Context
 import android.util.Log
-import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.BaseAdapter
+import android.widget.Filter
+import android.widget.Filterable
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.QuerySnapshot
 import edu.rosehulman.gearlocker.Constants
-import edu.rosehulman.gearlocker.R
 import edu.rosehulman.gearlocker.models.Club
 
 
 class ClubsAdapter(private val context: Context
-) : RecyclerView.Adapter<ClubsViewHolder>() {
-    private var clubs: ArrayList<Club> = ArrayList<Club>()
+) : BaseAdapter(), Filterable{
+    var clubs: ArrayList<Club> = ArrayList<Club>()
 
     private val currentClubsRef = FirebaseFirestore
         .getInstance()
@@ -41,33 +41,52 @@ class ClubsAdapter(private val context: Context
             when (change.type) {
                 DocumentChange.Type.ADDED -> {
                     clubs.add(0, club)
-                    notifyItemInserted(0)
+                    //notifyItemInserted(0)
                 }
                 DocumentChange.Type.REMOVED -> {
                     val position = clubs.indexOfFirst { it.id == club.id }
                     clubs.removeAt(position)
-                    notifyItemRemoved(position)
+                    //notifyItemRemoved(position)
                 }
                 DocumentChange.Type.MODIFIED -> {
                     val position= clubs.indexOfFirst { it.id == club.id }
                     clubs[position] = club
-                    notifyItemChanged(position)
+                    //notifyItemChanged(position)
                 }
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClubsViewHolder {
-        val view: CardView=
-            LayoutInflater.from(context).inflate(R.layout.club_cardview, parent, false) as CardView
-        return ClubsViewHolder(view, context)
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClubsViewHolder {
+//        val view: CardView=
+//            LayoutInflater.from(context).inflate(R.layout.club_cardview, parent, false) as CardView
+//        return ClubsViewHolder(view, context)
+//    }
+//
+//    override fun getItemCount(): Int= clubs.size
+//
+//    override fun onBindViewHolder(holder: ClubsViewHolder, position: Int) {
+//        holder.bind(clubs[position])
+//    }
+
+    override fun getFilter(): Filter {
+        TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int= clubs.size
-
-    override fun onBindViewHolder(holder: ClubsViewHolder, position: Int) {
-        holder.bind(clubs[position])
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("Not yet implemented")
     }
+
+    override fun getItem(position: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemId(position: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCount(): Int = clubs.size
+
 
 
 }
--- app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsFragment.kt
+++ app/src/main/java/edu/rosehulman/gearlocker/clubs/ClubsFragment.kt
@@ -1,32 +1,201 @@
 package edu.rosehulman.gearlocker.clubs
 
+import android.app.SearchManager
+import android.database.MatrixCursor
 import android.os.Bundle
+import android.provider.BaseColumns
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.CursorAdapter
+import android.widget.SearchView
+import android.widget.SimpleCursorAdapter
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
+import edu.rosehulman.gearlocker.Constants
 import edu.rosehulman.gearlocker.R
+import edu.rosehulman.gearlocker.models.Club
+import kotlinx.android.synthetic.main.search.view.*
 
 class ClubsFragment : Fragment() {
 
-    private var adapter: ClubsAdapter? =null
+    var clubs: ArrayList<Club> = ArrayList<Club>()
+
+    private val currentClubsRef = FirebaseFirestore
+        .getInstance()
+        .collection(Constants.FB_CLUBS)
+
+    init {
+        currentClubsRef.addSnapshotListener { snapshot, exception ->
+            handleSnapshotEvent(snapshot, exception)
+        }
+
+    }
+
+    private fun handleSnapshotEvent(
+        snapshot: QuerySnapshot?,
+        exception: FirebaseFirestoreException?
+    ) {
+        if (exception != null) {
+            Log.e(Constants.TAG, "Club Listen Error: $exception")
+            return
+        }
+
+        for (change in snapshot!!.documentChanges) {
+            val club = Club.fromSnapshot(change.document)
+
+            when (change.type) {
+                DocumentChange.Type.ADDED    -> {
+                    clubs.add(0, club)
+                }
+                DocumentChange.Type.REMOVED  -> {
+                    val position = clubs.indexOfFirst { it.id == club.id }
+                    clubs.removeAt(position)
+                }
+                DocumentChange.Type.MODIFIED -> {
+                    val position = clubs.indexOfFirst { it.id == club.id }
+                    clubs[position] = club
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//       val constraintLayout = inflater.inflate(R.layout.search, container, false)
+       val constraintLayout = inflater.inflate(R.layout.search, container, false)
+        val from = arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1)
+        val to = intArrayOf(R.id.club_textview)
+
+        val cursorAdapter = SimpleCursorAdapter(
+            context,
+            R.layout.club_cardview,
+            null,
+            from,
+            to,
+            CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        )
+        //cursorAdapter.setViewResource(R.id.add_club)
+//        cursorAdapter.setViewBinder { view, cursor, columnIndex ->
+//            view.add_club_textbutton.setOnClickListener {
+//                val button: TextView = view.add_club_textbutton
+//                val club: Club = clubs[cursor.position]
+//
+//
+//                val auth = FirebaseAuth.getInstance().currentUser
+//                if (club.members.containsValue(auth?.uid)) {
+//                    button.text = requireContext().getString(R.string.leave_club)
+//                    button.setBackgroundResource(R.drawable.round_button_red)
+//                    button.setOnClickListener {
+//                        removeFromClub(club, auth)
+//                    }
+//                } else {
+//                    button.text = requireContext().getString(R.string.add_club)
+//                    button.setBackgroundResource(R.drawable.round_button)
+//                    button.setOnClickListener {
+//                        addToClub(club, auth)
+//                    }
+//                }
+//            }
+//            return@setViewBinder true
+//        }
+        cursorAdapter.notifyDataSetChanged()
+        constraintLayout.searchView.suggestionsAdapter = cursorAdapter
+        constraintLayout.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                val cursor =
+                    MatrixCursor(arrayOf(BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1))
+                query?.let {
+                    clubs.forEachIndexed { index, suggestion ->
+                        if (suggestion.name.contains(query, true))
+                            cursor.addRow(arrayOf(index, suggestion.name))
+                    }
+                }
+                cursorAdapter.changeCursor(cursor)
+                return true
+            }
+        })
+
+        constraintLayout.searchView.setOnSuggestionListener(object : SearchView.OnSuggestionListener {
+            override fun onSuggestionSelect(position: Int): Boolean {
+                return false
+            }
+
+            override fun onSuggestionClick(position: Int): Boolean {
+//                val cursor = constraintLayout.searchView.suggestionsAdapter.getItem(position) as Cursor
+//                val selection =
+//                    cursor.getString(cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1))
+//                constraintLayout.searchView.setQuery(selection, false)
+//
+//                // Do something with selection
+                return true
+            }
+
+        })
+
+//        val constraintLayout : ConstraintLayout =
+//            inflater.inflate(R.layout.add_club_home, container, false) as ConstraintLayout
 //        adapter = ClubsAdapter(requireContext())
-//        constraintLayout.listView.adapter = adapter
-        val constraintLayout : ConstraintLayout =
-            inflater.inflate(R.layout.add_club_home, container, false) as ConstraintLayout
-        adapter = ClubsAdapter(requireContext())
-        constraintLayout.findViewById<RecyclerView>(R.id.listView).adapter = adapter
-        constraintLayout.findViewById<RecyclerView>(R.id.listView).layoutManager = LinearLayoutManager(context)
+//        constraintLayout.findViewById<RecyclerView>(R.id.listView).adapter = adapter
+//        constraintLayout.findViewById<RecyclerView>(R.id.listView).layoutManager = LinearLayoutManager(context)
         return constraintLayout
+    }
+
+    private fun addToClub(
+        club: Club,
+        auth: FirebaseUser?
+    ) {
+        if (auth != null) {
+            val displayName = if (!auth.displayName.isNullOrEmpty()) {
+                auth.displayName!!
+            } else {
+                auth.uid
+            }
+            club.members.putIfAbsent(displayName, auth.uid)
+            Log.d(Constants.TAG, club.members.toString())
+            FirebaseFirestore
+                .getInstance()
+                .collection(Constants.FB_CLUBS)
+                .document(club.id)
+                .set(club)
+
+
+        }
+    }
+
+    private fun removeFromClub(
+        club: Club,
+        auth: FirebaseUser?
+    ) {
+        if (auth != null) {
+            val displayName = if (!auth.displayName.isNullOrEmpty()) {
+                auth.displayName!!
+            } else {
+                auth.uid
+            }
+
+            if (!club.members.containsKey(displayName)) {
+                return
+            }
+
+            club.members.remove(displayName)
+            Log.d(Constants.TAG, club.members.toString())
+            FirebaseFirestore
+                .getInstance()
+                .collection(Constants.FB_CLUBS)
+                .document(club.id)
+                .set(club)
+        }
     }
 }
